e6e44c10baf8688b2ca9126368e4f9de5595218a
Mike Daley <michaelcdaley@yahoo.com>
2016-04-10 17:03:20 -0700

    Display error message if a task title is not specified

--- app/assets/javascripts/components/TaskBoard.js.jsx
+++ app/assets/javascripts/components/TaskBoard.js.jsx
@@ -316,7 +316,7 @@ var Task = React.createClass({
 var TaskForm = React.createClass({
   
   getInitialState: function() {
-    return { title: '', description: '', due_text: ''}
+    return { title: '', description: '', due_text: '', errors: {} }
   },
   
   handleChange: function(event) {
@@ -346,8 +346,26 @@ var TaskForm = React.createClass({
         this.props.handleNewTask( data["task"]            );
         this.setState(            this.getInitialState()  );
       }.bind(this),
+      error: function(xhr, status, err) {
+        //this.handleSubmitError(xhr, status, err);
+        console.error('/tasks', status, err.toString() );
+        this.setState( { errors: xhr["responseJSON"] } );
+      }.bind(this),
       dataType: 'JSON'
     });
+  },
+  
+  handleSubmitError: function(xhr, status, err) {
+    console.error('/tasks', status, err.toString() );
+    this.setState( { errors: xhr["responseJSON"] } );
+    return;
+  },
+  
+  hasError: function(name) {
+    if(this.state.errors.hasOwnProperty(name) ) {
+      return true;
+    }
+    return false;
   },
   
   handleClear: function(e) {
@@ -366,50 +384,62 @@ var TaskForm = React.createClass({
   taskForm: function() {
     console.log('[TaskForm] render()');
     
+    {/*
+      TODO: 04/09/2016
+      -  TO ADD SIMPLE ERROR HANDLING, I CAN ADD THE 'has-error' CLASS TO THE 
+         FORM-GROUP AND I CAN REPLACE THE PLACEHOLDER WITH THE ERROR MESSAGE.
+         OTHERWISE, I COULD ADD A SPAN TO THE BOTTOM THE FORM-CONTROL THAT IS
+         ONLY DISPLAYED WHEN THERE IS AN ERROR.
+    */}
     return (
-      <li className="list-group-item">
-        <div className="row">
-          <div className="col-sm-12 col-md-12 col-lg-12">
-            
-            <form onSubmit={this.handleSubmit}>
-      
-              <div className='form-group'>
-                <label>   Title </label>
-                <input    type          = 'text'   
-                          className     = 'form-control' 
-                          name          = 'title'  
-                          placeholder   = 'Enter task'
-                          value         = {this.state.title}
-                          onChange      = {this.handleChange} />
-              </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-12 col-lg-12">
           
-              <div className='form-group'>
-                <label>   Description </label>
-                <textarea type          = 'text'         
-                          className     = 'form-control' 
-                          name          = 'description'  
-                          placeholder   = 'Enter task description'
-                          value         = {this.state.description}
-                          onChange      = {this.handleChange} />
-              </div>
-                        
-              <div className='form-group'>
-                <label>   Due </label>
-                <input    type          = 'text'   
-                          className     = 'form-control' 
-                          name          = 'due_text'    
-                          placeholder   = "Enter due date"
-                          value         = { this.state.due_text }
-                          onChange      = { this.handleChange   } />
-              </div>
-                    
-              <button className="btn btn-primary" type="submit" >              Submit </button>
-              <button className="btn btn-default" onClick={this.handleClear}>  Clear  </button>
-            </form>
+          <form>
+            <div className={ this.hasError('title') ? 'form-group has-error' : 'form-group'}>
+              <label>   Title* </label>
+              <input    type          = 'text'   
+                        className     = 'form-control' 
+                        name          = 'title'  
+                        placeholder   = 'Enter task'
+                        value         = {this.state.title}
+                        onChange      = {this.handleChange} />
+
+              <span className={this.hasError('title') ? 'error-msg' : 'hidden'}> 
+                { this.hasError('title') ? this.state.errors['title'][0] : '' } 
+              </span>
+            </div>
+        
+            <div className='form-group'>
+              <label>   Description </label>
+              <textarea type          = 'text'         
+                        className     = 'form-control' 
+                        name          = 'description'  
+                        placeholder   = 'Enter task description'
+                        value         = {this.state.description}
+                        onChange      = {this.handleChange} />
+            </div>
                       
-          </div>
+            <div className='form-group'>
+              <label>   Due </label>
+              <input    type          = 'text'   
+                        className     = 'form-control' 
+                        name          = 'due_text'    
+                        placeholder   = "Enter due date"
+                        value         = { this.state.due_text }
+                        onChange      = { this.handleChange   } />
+            </div>
+                  
+            <button type      = "submit" 
+                    className = "btn btn-primary" 
+                    onClick   = {this.handleSubmit} > Submit  </button>
+            
+            <button className = "btn btn-default"
+                    onClick   = {this.handleClear}  >  Clear  </button>
+          </form>
+                    
         </div>
-      </li>
+      </div>
     );
   },
   
