14270ba28cf7e71d201b8658bff396b14bb82dde
Mike Daley <michaelcdaley@yahoo.com>
2016-04-21 11:02:58 -0700

    Modified tasks#show to use the TaskShow reactjs component to display the page

---
+++ app/assets/javascripts/components/TaskShow.js.jsx
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * File: assets/javascripts/components/TaskShow.js.jsx
+ ******************************************************************************/
+
+var TaskShow = React.createClass({
+  getInitialState: function() {
+    return { task: {} };
+  },
+  
+  componentWillMount: function() {
+    this.setState( { task: this.props.data } );
+  },
+  
+  updateTask: function(task, data) {
+    console.log('[TaskShow] updateTask()');
+    
+    this.setState( { task: data["task"] } );
+    
+    return;
+  },
+
+  render: function() {
+    var title = this.props.title || "Hello World";
+    
+    return (
+      <div className="task-show">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-12">
+            <TaskHeader   title = { title } />
+          </div>
+        </div>
+      
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-12">
+            <Task task            = { this.state.task     }
+                  key             = { this.state.task.id  }
+                  handleCheckTask = { this.updateTask     }
+                  handleEditTask  = { this.updateTask     } />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+});  // end of TaskShow
